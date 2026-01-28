7e14d5b862bb1c6d4ac8c61b879d5c314c184ab0
RizqyNurfauzella <fauzella171@gmail.com>
2026-01-28 19:39:15 +0700

    Configuration Tailwind CSS

--- tellyou/tailwind.config.js
+++ tellyou/tailwind.config.js
@@ -11,6 +11,24 @@ export default {
     ],
     theme: {
         extend: {
+            colors: {
+                'telkom-red': '#B91C1C',
+                'telkom-maroon': '#7C2D12',
+                'telkom-white': '#FFFFFF',
+                'telkom-black': '#000000',
+                'telkom-gray': {
+                    50: '#f9fafb',
+                    100: '#f3f4f6',
+                    200: '#e5e7eb',
+                    300: '#d1d5db',
+                    400: '#9ca3af',
+                    500: '#6b7280',
+                    600: '#4b5563',
+                    700: '#374151',
+                    800: '#1f2937',
+                    900: '#111827',
+                },
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
